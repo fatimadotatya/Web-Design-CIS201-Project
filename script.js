750d09ebbbbac1c2e4f93b5d14b0b2112edbebd4
fatimadotatya <fatima.atya2006@gmail.com>
2026-05-02 20:10:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function showMessage() {
-  document.getElementById("message").textContent = "Welcome to our cafe!";
+  document.getElementById("message").textContent = "Thank you for visiting our vafe!";
 }
